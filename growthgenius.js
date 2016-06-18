3ef12eeb498e7c01e61c851ed77a2c42326a9955
Brandon Pizzacalla <bpizzacalla@gmail.com>
2016-06-17 20:54:12 -0700

    quick commit

--- growthgenius.js
+++ growthgenius.js
@@ -1,4 +1,4 @@
-var versionNumber = { version: 20 } // to reset file cache forefully on prod. Updated with shell script.
+var versionNumber = { version: 21 } // to reset file cache forefully on prod. Updated with shell script.
 
 // // For reference, load in SquareSpace directly
 
@@ -67,12 +67,11 @@ $(document).ready(function() {
 
   // Reset form (hide / show appropriate elements, reset values .reset(), etc)
   var contactReset = function() {
-    var htmlVrsLocal = $(this).get(0) // Need .get(0) to return html element (instead of $jQuery object) 
     console.log("contact reset $this:"); console.log($(this))
     $('.thank-you').fadeOut('fast')
     $('.thank-you').remove()
     $(this).find('.none1, .none0').hide() // $this refers to $('form') that envoked it.
-    htmlVrsLocal.reset() // reset this form. Need .get(0) to return html element (instead of $jQuery object)
+    $(this).find('input[type="text"]').val("") // reset this form.
     ( !$(this).hasClass('form-style-lower') ) ? $(this).find('input[name="message"]').eq(0).prop('style').width =  "20%" : null ; // only apply width styling to navbar form
     $(this).find('input[name="message"]').prop('placeholder', 'Type here...')
     $(this).find('input').prop('required', false)
